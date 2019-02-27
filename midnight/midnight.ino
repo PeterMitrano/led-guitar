92216faab89489776375f79db564a5c658e72bc2
Peter Mitrano <mitranopeter@gmail.com>
2019-02-27 01:41:52 -0500

    got fret reading and designed new input schema

--- midnight/midnight.ino
+++ midnight/midnight.ino
@@ -1,120 +1,165 @@
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
-#include <song.h>
 
-constexpr int outline_pin = 3;
-constexpr int top_pin = 5;
-constexpr int middle_pin = 6;
-constexpr int bottom_pin = 0;
+struct OutlineEvent {
+  uint8_t onset;
+  uint8_t midi_number;
+};
+
+struct FretEvent {
+  uint8_t onset;
+  uint8_t string_number;
+  uint8_t fret_number;
+};
+
+#include "solo.h"
+
+constexpr auto outline_pin{3u};
+constexpr auto top_pin{5u};
+constexpr auto middle_pin{6u};
+constexpr auto bottom_pin{0u};
 
 Adafruit_NeoPixel outline = Adafruit_NeoPixel(60, outline_pin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel top_strip = Adafruit_NeoPixel(60, top_pin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel middle_strip = Adafruit_NeoPixel(60, middle_pin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel bottom_strip = Adafruit_NeoPixel(60, bottom_pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel top = Adafruit_NeoPixel(60, top_pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel middle = Adafruit_NeoPixel(60, middle_pin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel bottom = Adafruit_NeoPixel(60, bottom_pin, NEO_GRB + NEO_KHZ800);
+
+auto time_step{0ull};
+auto fret_events_idx{0u};
+auto outline_events_idx{0u};
 
 void setup() {
   outline.begin();
-  top_strip.begin();
-  middle_strip.begin();
-  bottom_strip.begin();
+  top.begin();
+  middle.begin();
+  bottom.begin();
 
   unsigned int const initial_brightness{50};
   outline.setBrightness(initial_brightness);
-  top_strip.setBrightness(initial_brightness);
-  middle_strip.setBrightness(initial_brightness);
-  bottom_strip.setBrightness(initial_brightness);
+  top.setBrightness(initial_brightness);
+  middle.setBrightness(initial_brightness);
+  bottom.setBrightness(initial_brightness);
 
   outline.show();
-  top_strip.show();
-  middle_strip.show();
-  bottom_strip.show();
+  top.show();
+  middle.show();
+  bottom.show();
 }
 
 void loop() {
-  delay(20)
+  auto current_outline_event = outline_events[outline_events_idx];
+
+  //  for(uint16_t i=0; i<outline.numPixels(); i++) {
+  //    auto color = outline.Color(solo[0][i][0], solo[0][i][1], solo[0][i][2]);
+  //    outline.setPixelColor(i, color);
+  //  }
+  //  for(uint16_t i=0; i<top.numPixels(); i++) {
+  //    auto color = top.Color(solo[0][i][0], solo[0][i][1], solo[0][i][2]);
+  //    top.setPixelColor(i, color);
+  //  }
+  //  for(uint16_t i=0; i<middle.numPixels(); i++) {
+  //    auto color = middle.Color(solo[0][i][0], solo[0][i][1], solo[0][i][2]);
+  //    middle.setPixelColor(i, color);
+  //  }
+  //  for(uint16_t i=0; i<bottom.numPixels(); i++) {
+  //    auto color = bottom.Color(solo[0][i][0], solo[0][i][1], solo[0][i][2]);
+  //    bottom.setPixelColor(i, color);
+  //  }
+
+  outline.show();
+  top.show();
+  middle.show();
+  bottom.show();
+
+  delayMicroseconds(us_per_dt);
+
+  ++time_step;
+  if (time_step == current_outline_event.onset) {
+    outline_events_idx++;
+  }
 }
 
 //// Fill the dots one after the other with a color
 //void colorWipe(uint32_t c, uint8_t wait) {
-  //for(uint16_t i=0; i<strip.numPixels(); i++) {
-    //strip.setPixelColor(i, c);
-    //strip.show();
-    //delay(wait);
-  //}
+//for(uint16_t i=0; i<strip.numPixels(); i++) {
+//strip.setPixelColor(i, c);
+//strip.show();
+//delay(wait);
+//}
 //}
 
 //void rainbow(uint8_t wait) {
-  //uint16_t i, j;
+//uint16_t i, j;
 
-  //for(j=0; j<256; j++) {
-    //for(i=0; i<strip.numPixels(); i++) {
-      //strip.setPixelColor(i, Wheel((i+j) & 255));
-    //}
-    //strip.show();
-    //delay(wait);
-  //}
+//for(j=0; j<256; j++) {
+//for(i=0; i<strip.numPixels(); i++) {
+//strip.setPixelColor(i, Wheel((i+j) & 255));
+//}
+//strip.show();
+//delay(wait);
+//}
 //}
 
 //// Slightly different, this makes the rainbow equally distributed throughout
 //void rainbowCycle(uint8_t wait) {
-  //uint16_t i, j;
+//uint16_t i, j;
 
-  //for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    //for(i=0; i< strip.numPixels(); i++) {
-      //strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    //}
-    //strip.show();
-    //delay(wait);
-  //}
+//for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+//for(i=0; i< strip.numPixels(); i++) {
+//strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+//}
+//strip.show();
+//delay(wait);
+//}
 //}
 
 ////Theatre-style crawling lights.
 //void theaterChase(uint32_t c, uint8_t wait) {
-  //for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    //for (int q=0; q < 3; q++) {
-      //for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        //strip.setPixelColor(i+q, c);    //turn every third pixel on
-      //}
-      //strip.show();
+//for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+//for (int q=0; q < 3; q++) {
+//for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+//strip.setPixelColor(i+q, c);    //turn every third pixel on
+//}
+//strip.show();
 
-      //delay(wait);
+//delay(wait);
 
-      //for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        //strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      //}
-    //}
-  //}
+//for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+//strip.setPixelColor(i+q, 0);        //turn every third pixel off
+//}
+//}
+//}
 //}
 
 ////Theatre-style crawling lights with rainbow effect
 //void theaterChaseRainbow(uint8_t wait) {
-  //for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    //for (int q=0; q < 3; q++) {
-      //for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        //strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      //}
-      //strip.show();
+//for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+//for (int q=0; q < 3; q++) {
+//for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+//strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+//}
+//strip.show();
 
-      //delay(wait);
+//delay(wait);
 
-      //for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        //strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      //}
-    //}
-  //}
+//for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+//strip.setPixelColor(i+q, 0);        //turn every third pixel off
+//}
+//}
+//}
 //}
 
 //// Input a value 0 to 255 to get a color value.
 //// The colours are a transition r - g - b - back to r.
 //uint32_t Wheel(byte WheelPos) {
-  //WheelPos = 255 - WheelPos;
-  //if(WheelPos < 85) {
-    //return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  //}
-  //if(WheelPos < 170) {
-    //WheelPos -= 85;
-    //return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  //}
-  //WheelPos -= 170;
-  //return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+//WheelPos = 255 - WheelPos;
+//if(WheelPos < 85) {
+//return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//}
+//if(WheelPos < 170) {
+//WheelPos -= 85;
+//return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//}
+//WheelPos -= 170;
+//return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 //}
--- midnight/xml_to_h.py
+++ midnight/xml_to_h.py
@@ -1,22 +1,102 @@
 #!/usr/bin/env python3
+import numpy as np
 import argparse
 import music21
 
+"""
+The range of notes in the solo is 18 half steps, and the neck of our guitar will have 20 LEDs,
+so we can just ignore the last two and map one-to-one. Therefore B3 maps to 0.
+"""
+
+num_leds = 60 * 3 + 30 * 2
+end_outline_leds = 60 * 3
+
+red = np.array([1, 0, 0])
+green = np.array([0, 1, 0])
+blue = np.array([0, 0, 1])
+
+
+class FretEvent:
+
+    def __index__(self):
+        self.onset = 0
+        self.string_number = 0
+        self.fret_number = 0
+
+    def __repr__(self):
+        return "({},{},{})".format(self.onset, self.string_number, self.fret_number)
+
+
+class Event:
+
+    def __index__(self):
+        self.onset = 0
+        self.midi_number = 0
+
+    def __repr__(self):
+        return "({},{})".format(self.onset, self.midi_number)
+
+
+def define_constant(vartype, name, value):
+    return "{} const {} = {};\n".format(vartype, name, value)
+
+
+def define_fret_events_array(events):
+    definition = "FretEvent const fret_events[] = {\n"
+    for e in events:
+        definition += "  FretEvent{" + "{}, {}, {}".format(e.onset, e.string_number, e.fret_number) + "},\n"
+    definition += "};"
+    return definition
+
+
+def define_outline_events_array(events):
+    definition = "OutlineEvent const outline_events[] = {\n"
+    for e in events:
+        definition += "  OutlineEvent{" + "{}, {}".format(e.onset, e.midi_number) + "},\n"
+    definition += "};"
+    return definition
+
 
 def main():
+    np.random.seed(0)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("infile")
+    parser.add_argument("outfile")
     parser.add_argument("--bpm", type=float, default=100)
     args = parser.parse_args()
 
-    score = music21.converter.parse(args.infile)
-    score = score.stripTies()
-    onsets = []
     seconds_per_quarter = 60.0 / args.bpm
-    for n in score.recurse().notes:
-        onsets.append(n.getOffsetInHierarchy(score) * seconds_per_quarter)
+    dt_per_quarter = 16
+    us_per_dt = int(seconds_per_quarter / dt_per_quarter * 1000)
 
-    print(onsets)
+    fret_events = []
+    outline_events = []
+
+    score = music21.converter.parse(args.infile)
+    fretting_part = score.parts[0].flat.stripTies()  # notated with tablature
+    for fret_note in fretting_part.notes:
+        fret_event = FretEvent()
+        fret_event.onset = int(fret_note.getOffsetInHierarchy(score) * dt_per_quarter)
+        for articulation in fret_note.articulations:
+            if 'FretIndication' in articulation.classSet:
+                fret_event.fret_number = int(articulation.number)
+            if 'StringIndication' in articulation.classSet:
+                fret_event.string_number = int(articulation.number)
+        fret_events.append(fret_event)
+
+    outline_part = score.parts[1].flat.stripTies()  # notated with a piano score
+    for outline_note in outline_part.notes:
+        outline_event = Event()
+        outline_event.onset = int(outline_note.getOffsetInHierarchy(score) * dt_per_quarter)
+        outline_event.midi_number = outline_note.pitch.midi
+        outline_events.append(outline_event)
+
+    with open(args.outfile, 'w') as outfile:
+        outfile.write(define_constant("unsigned int", "us_per_dt", us_per_dt))
+        outfile.write(define_outline_events_array(outline_events))
+        outfile.write(define_fret_events_array(fret_events))
+        outfile.write("\n")
 
 
 if __name__ == '__main__':
