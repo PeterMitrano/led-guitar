0c035c5b75a694617ad941949448fbc22ed8a06d
Peter Mitrano <mitranopeter@gmail.com>
2019-02-28 18:04:20 -0500

    tested power consumption all LEDs

---
+++ README.md
@@ -0,0 +1,4 @@
+# Powering
+
+ - current draw for all 240 LEDs on full brightess is 3.4 amps
+ - individual 60 led strip neverdraws more than 890ma
--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -5,31 +5,82 @@
 
 #define PIN 6
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(30, 3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(30, 11, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(60, 5, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip4 = Adafruit_NeoPixel(60, 6, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip5 = Adafruit_NeoPixel(60, 9, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip6 = Adafruit_NeoPixel(60, 10, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  strip.begin();
-  strip.setBrightness(50);
-  strip.show(); // Initialize all pixels to 'off'
+  strip1.begin();
+  strip2.begin();
+  strip3.begin();
+  strip4.begin();
+  strip5.begin();
+  constexpr auto B{100};
+  strip1.setBrightness(B);
+  strip2.setBrightness(B);
+  strip3.setBrightness(B);
+  strip4.setBrightness(B);
+  strip5.setBrightness(B);
+  // Initialize all pixels to 'off'
+  strip1.show();
+  strip2.show();
+  strip3.show();
+  strip4.show();
+  strip5.show();
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127, 0, 0), 50); // Red
-  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+  uint16_t i, j;
 
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip1.numPixels(); i++) {
+      strip1.setPixelColor(i, 0, 255, 255);
+    }
+    strip1.show();
+
+    for(i=0; i<strip2.numPixels(); i++) {
+      strip2.setPixelColor(i, 255, 0, 255);
+    }
+    strip2.show();
+
+    for(i=0; i<strip3.numPixels(); i++) {
+      strip3.setPixelColor(i, 255, 0, 0);
+    }
+    strip3.show();
+
+    for(i=0; i<strip4.numPixels(); i++) {
+      strip4.setPixelColor(i, 0, 255, 0);
+    }
+    strip4.show();
+
+    for(i=0; i<strip5.numPixels(); i++) {
+      strip5.setPixelColor(i, 255, 255, 255);
+    }
+    strip5.show();
+
+    delay(2);
+  }
+
+  //rainbow(strip1, 20);
+  //rainbow(strip2, 20);
+  //rainbow(strip3, 20);
+  //rainbow(strip4, 20);
+  //rainbow(strip5, 20);
+  //rainbowCycle(20);
+  //theaterChaseRainbow(50);
+  //colorWipe(strip.Color(255, 0, 0), 50); // Red
+  //colorWipe(strip.Color(0, 255, 0), 50); // Green
+  //colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  //theaterChase(strip.Color(127, 127, 127), 50); // White
+  //theaterChase(strip.Color(127, 0, 0), 50); // Red
+  //theaterChase(strip.Color(0, 0, 127), 50); // Blue
 }
 
 // Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
+void colorWipe(Adafruit_NeoPixel &strip, uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
     strip.show();
@@ -37,12 +88,12 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-void rainbow(uint8_t wait) {
+void rainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
   uint16_t i, j;
 
   for(j=0; j<256; j++) {
     for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+      strip.setPixelColor(i, Wheel(strip, (i+j) & 255));
     }
     strip.show();
     delay(wait);
@@ -50,12 +101,12 @@ void rainbow(uint8_t wait) {
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
+void rainbowCycle(Adafruit_NeoPixel &strip, uint8_t wait) {
   uint16_t i, j;
 
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      strip.setPixelColor(i, Wheel(strip, ((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
     delay(wait);
@@ -63,7 +114,7 @@ void rainbowCycle(uint8_t wait) {
 }
 
 //Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
+void theaterChase(Adafruit_NeoPixel &strip, uint32_t c, uint8_t wait) {
   for (int j=0; j<10; j++) {  //do 10 cycles of chasing
     for (int q=0; q < 3; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
@@ -81,11 +132,11 @@ void theaterChase(uint32_t c, uint8_t wait) {
 }
 
 //Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
+void theaterChaseRainbow(Adafruit_NeoPixel &strip, uint8_t wait) {
   for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
     for (int q=0; q < 3; q++) {
       for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        strip.setPixelColor(i+q, Wheel(strip, (i+j) % 255));    //turn every third pixel on
       }
       strip.show();
 
@@ -100,7 +151,7 @@ void theaterChaseRainbow(uint8_t wait) {
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t Wheel(Adafruit_NeoPixel &strip, byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
     return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
